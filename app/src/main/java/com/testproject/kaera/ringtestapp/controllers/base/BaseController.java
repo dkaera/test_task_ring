06a41c3cbaf03bf471e2d2ddded6c70c3b122cb9
Puzak Dmytro <dmytro.puzak@globallogic.com>
2018-04-05 15:02:41 +0300

    Implement Conductor

---
+++ app/src/main/java/com/testproject/kaera/ringtestapp/controllers/base/BaseController.java
@@ -0,0 +1,52 @@
+package com.testproject.kaera.ringtestapp.controllers.base;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+
+import com.bluelinelabs.conductor.Controller;
+import com.testproject.kaera.ringtestapp.ui.ActionBarProvider;
+
+public abstract class BaseController extends RefWatchingController {
+
+    public BaseController() {
+    }
+
+    public BaseController(Bundle args) {
+        super(args);
+    }
+
+    // Note: This is just a quick demo of how an ActionBar *can* be accessed, not necessarily how it *should*
+    // be accessed. In a production app, this would use Dagger instead.
+    protected ActionBar getActionBar() {
+        ActionBarProvider actionBarProvider = ((ActionBarProvider)getActivity());
+        return actionBarProvider != null ? actionBarProvider.getSupportActionBar() : null;
+    }
+
+    @Override
+    protected void onAttach(@NonNull View view) {
+        setTitle();
+        super.onAttach(view);
+    }
+
+    protected void setTitle() {
+        Controller parentController = getParentController();
+        while (parentController != null) {
+            if (parentController instanceof BaseController && ((BaseController)parentController).getTitle() != null) {
+                return;
+            }
+            parentController = parentController.getParentController();
+        }
+
+        String title = getTitle();
+        ActionBar actionBar = getActionBar();
+        if (title != null && actionBar != null) {
+            actionBar.setTitle(title);
+        }
+    }
+
+    protected String getTitle() {
+        return null;
+    }
+}
