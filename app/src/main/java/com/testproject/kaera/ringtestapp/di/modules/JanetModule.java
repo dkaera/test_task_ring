f65bed942cbe0a79fd736f7e2a12c4a960bdaf8a
Puzak Dmytro <dmytro.puzak@globallogic.com>
2018-04-12 12:51:10 +0300

    Add GSON

--- app/src/main/java/com/testproject/kaera/ringtestapp/di/modules/JanetModule.java
+++ app/src/main/java/com/testproject/kaera/ringtestapp/di/modules/JanetModule.java
@@ -1,36 +1,21 @@
 package com.testproject.kaera.ringtestapp.di.modules;
 
-import android.app.Application;
-import android.content.Context;
-
-import com.grasshopper.dialer.service.util.ActionDataProvider;
-import com.grasshopper.dialer.service.util.CommandServiceWrapper;
-import com.grasshopper.dialer.service.util.TimberServiceWrapper;
-
-import java.util.Set;
-
 import dagger.Module;
 import dagger.Provides;
 import io.techery.janet.ActionService;
 import io.techery.janet.CommandActionService;
 import io.techery.janet.Janet;
-import io.techery.presenta.di.ApplicationScope;
 
 @Module
 public class JanetModule {
 
     @Provides
-    public ActionService provideCommandService(Context context) {
-        return new CommandServiceWrapper(new CommandActionService(), context);
+    public ActionService provideCommandService() {
+        return new CommandActionService();
     }
 
     @Provides
-    Janet provideJanet(Set<ActionService> services, Application application) {
-        Janet.Builder builder = new Janet.Builder();
-
-        for (ActionService service : services) {
-            builder.addService((new ActionDataProvider(new TimberServiceWrapper(service), application)));
-        }
-        return builder.build();
+    Janet provideJanet() {
+        return new Janet.Builder().build();
     }
 }
